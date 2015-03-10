21f8defd33169ecb33246cfddfa410cf91d6a651
agus4171 <agus.4171.wirawan@gmail.com>
2015-03-10 23:57:07 +0000

    server sudah dapat menangani banyak client

--- chatapp_server.c
+++ chatapp_server.c
@@ -1,28 +1,34 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
-#include <string.h>
 #include <unistd.h>
- 
+#include <pthread.h>
+
+#define PORT 8888
+
+void *connection_handler(void *);
+
 int main(int argc, char *argv[])
 {
-	int socket_desc, client_sock, c, read_size;
+    int socket_desc, client_sock, c, *new_sock;
 	struct sockaddr_in server, client;
-	char client_message[2000];
-	
+	//char client_message[2000];
+
 	socket_desc = socket(AF_INET, SOCK_STREAM, 0);
-	
-	//Create socket
+
+	//membuat socket
 	if (socket_desc == -1)
 	{
 		printf("Could not create socket\n");
 	}
 	printf("Socket created\n");
-	
-	//Prepare the sockaddr_in structure
+
+	//deklarasi ip version dan port yang digunakan
 	server.sin_family = AF_INET;
 	server.sin_addr.s_addr = INADDR_ANY;
-	server.sin_port = htons(9000);
+	server.sin_port = htons(PORT);
 
 	//Bind
 	if(bind(socket_desc,(struct sockaddr *)&server, sizeof(server)) < 0)
@@ -30,23 +36,47 @@ int main(int argc, char *argv[])
 		printf("Bind failed\n");
 		return 1;
 	}
-	printf("Bind done");
-	
+	printf("Bind done\n");
+
 	listen(socket_desc, 3);
 
-	//Accetp and incoming connection
+	//menunggu koneksi dari client
 	printf("Waiting for incoming connections\n");
 	c = sizeof(struct sockaddr_in);
-	
-	//Accept connection from an incoming cllient
-	client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c);
-	if(client_sock < 0)
+	printf("sizeof sockaddr_in : %d", c);
+
+	//menerima koneksi dari client
+	while( (client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c)) )
+	/*if(client_sock < 0)
 	{
 		printf("Accept failed\n");
 		return 1;
+	}*/
+	{
+        printf("client_sock : %d socket_desc : %d", client_sock, socket_desc);
+        printf("Connection accepted\n");
+        printf("ip client : %s port : %d\n", inet_ntoa(client.sin_addr), ntohs(client.sin_port));
+        pthread_t sniffer_thread;
+        new_sock = malloc(1);
+        *new_sock = client_sock;
+        //printf();
+        if(pthread_create( &sniffer_thread, NULL, connection_handler, (void*) new_sock) < 0)
+        {
+            printf("Could not create thread\n");
+            return 1;
+        }
+        printf("Handler assigned\n");
+
 	}
-	printf("Connection accepted");
-	
+
+	if(client_sock < 0)
+	{
+        printf("Accept failed");
+        return 1;
+	}
+	return 0;
+	/*printf("Connection accepted");
+
 	while((read_size == recv(client_sock, client_message, 2000, 0)) > 0)
 	{
 		write(client_sock, client_message, strlen(client_message));
@@ -61,5 +91,35 @@ int main(int argc, char *argv[])
 	{
 		printf("Receive failed");
 	}
-	return 0;
+	return 0;*/
+}
+
+void *connection_handler(void *socket_desc)
+{
+    //membuat socket decriptor
+    int sock = *(int*)socket_desc;
+    int read_size;
+    char *message, client_message[2000];
+
+    //mengirimkan pesan ke client bahwa dia sudah berhasil terhubung dengan server
+    message = "Selamat, saya sudah menghandel koneksi Anda\n";
+    write(sock, message, strlen(message));
+
+    while( (read_size = recv(sock, client_message, 2000, 0)) >0 )
+    {
+        write(sock, client_message, strlen(client_message));
+    }
+
+    if(read_size == 0)
+    {
+        printf("Client disconnect\n");
+        fflush(stdout);
+    }
+    else if(read_size == -1)
+    {
+        printf("recv failed");
+    }
+
+    free(socket_desc);
+    return 0;
 }
